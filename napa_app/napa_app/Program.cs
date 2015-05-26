c36aa9ec06c36a3c46bca5734dc0573e52f56521
Chris Cummings <ccummings@napa.com>
2015-05-26 15:34:48 -0400

    save results to results collection

--- napa_app/napa_app/Program.cs
+++ napa_app/napa_app/Program.cs
@@ -4,25 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AlchemyAPI;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace napa_app
 {
     class Program
     {
+        public async static Task DoStuff(string json)
+        {
+            var client = new MongoClient("mongodb://napa_user:Password0@ds031982.mongolab.com:31982/heroku_app37054022");
+
+            var db = client.GetDatabase("heroku_app37054022");
+            var collection = db.GetCollection<BsonDocument>("results");
+            MongoDB.Bson.BsonDocument document = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(json);
+            await collection.InsertOneAsync(document);
+        }
+
         static void Main(string[] args)
         {
             AlchemyAPI.AlchemyAPI alchemyObj = new AlchemyAPI.AlchemyAPI();
-            alchemyObj.SetAPIKey("6b5aa8499e9a98215e57cf2ef6a0456654be777b");
-            //alchemyObj.SetAPIKey("4c32fab89d526ce5e132a82213f68daf67c5d266");
+            //alchemyObj.SetAPIKey("6b5aa8499e9a98215e57cf2ef6a0456654be777b");
+            alchemyObj.SetAPIKey("4c32fab89d526ce5e132a82213f68daf67c5d266");
 
             AlchemyAPI_NewsParams prms = new AlchemyAPI_NewsParams();
+            prms.setOutputMode(AlchemyAPI_BaseParams.OutputMode.JSON);
             prms.StartDate = "now-30d";
             prms.EndDate = "now";
             prms.Count = 10;
             prms.setEntities(new Entity(EntityType.Person, "Elon Musk"));
-            string xml = alchemyObj.GetNews(prms);
+            string json = alchemyObj.GetNews(prms);
 
-            Console.WriteLine(xml);
+            //var task = DoStuff(json);
+            //task.Wait();
+
+            Console.WriteLine(json);
             Console.ReadLine();
 
         }
