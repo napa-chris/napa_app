8d0ada096203ac8af05749fcb3a2a37106baf454
napa-chris <ccummings@napa.com>
2015-05-26 16:33:41 -0400

    Revert "Moving Stable into Master"

--- napa_app/napa_app/Program.cs
+++ napa_app/napa_app/Program.cs
@@ -4,31 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AlchemyAPI;
-using napa_app.sdk;
-using MongoDB.Driver;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.IO;
 
 namespace napa_app
 {
     class Program
     {
-        public async static Task SaveResults(string json)
-        {
-            var client = new MongoClient("mongodb://napa_user:Password0@ds031982.mongolab.com:31982/heroku_app37054022");
-            var db = client.GetDatabase("heroku_app37054022");
-            var collection = db.GetCollection<BsonDocument>("results");
-            BsonDocument document = BsonSerializer.Deserialize<BsonDocument>(json);
-            var status = document.GetElement("status");
-            if (status.Value != "ERROR")
-            {
-                var results = document.GetElement("result").Value.AsBsonDocument.GetElement("docs").Value.AsBsonArray.Select(a => a.AsBsonDocument);
-                await collection.InsertManyAsync(results);
-                Console.WriteLine("Saved");
-            }
-        }
-
         static void Main(string[] args)
         {
             AlchemyAPI.AlchemyAPI alchemyObj = new AlchemyAPI.AlchemyAPI();
@@ -36,19 +16,18 @@ namespace napa_app
             //alchemyObj.SetAPIKey("4c32fab89d526ce5e132a82213f68daf67c5d266");
 
             AlchemyAPI_NewsParams prms = new AlchemyAPI_NewsParams();
-            prms.setOutputMode(AlchemyAPI_BaseParams.OutputMode.JSON);
-            prms.StartDate = "now-30d";
-            prms.EndDate = "now";
-            prms.Count = 10;
-            prms.setEntities(new Entity(EntityType.Person, "Elon Musk"));
-            prms.setReturn(ReturnOutputData.Url, ReturnOutputData.Title, ReturnOutputData.Taxonomy);
-            string json = alchemyObj.GetNews(prms);
+            prms.setStartDate(30, 'd');
+            prms.setEndDate();
+            //string[] taxonomy = { "books" };
+            string[] entityText = { "elon+musk" };
+            //prms.addTaxonomy(taxonomy);
+            prms.addEntityText(entityText);
+            prms.setCount(10);
+            string xml = alchemyObj.GetNews(prms);
 
-            var task = SaveResults(json);
-            task.Wait();
-
-            Console.WriteLine("Done.");
+            Console.WriteLine(xml);
             Console.ReadLine();
+
         }
     }
 }
--- napa_app/napa_app/sdk/AlchemyAPI_NewsParams.cs
+++ napa_app/napa_app/sdk/AlchemyAPI_NewsParams.cs
@@ -7,95 +7,131 @@ namespace AlchemyAPI
 {
     public class AlchemyAPI_NewsParams : AlchemyAPI_BaseParams
     {
-        private const string TAXONOMY_LABEL = "q.enriched.url.enrichedTitle.taxonomy.taxonomy_.label";
-        private const string CONCEPT_TEXT = "q.enriched.url.concepts.concept.text";
-        private const string KEYWORD_TEXT = "q.enriched.url.enrichedTitle.keywords.keyword.text";
-        private const string ENTITY_TEXT = "q.enriched.url.enrichedTitle.entities.entity.text";
-        private const string ENTITY_TYPE = "q.enriched.url.enrichedTitle.entities.entity.type";
-        private const string RELATION = "q.enriched.url.enrichedTitle.relations.relation";
-        private const string SENTIMENT = "q.enriched.url.enrichedTitle.docSentiment";
-        private const string COUNT = "count";
-        private const string START_DATE = "start";
-        private const string END_DATE = "end";
-        private const string RETURN = "&return";
-
-        private readonly Dictionary<string, string> _commands;
-
-        public String StartDate
+        public enum SentimentType
         {
-            set { _commands[START_DATE] = value; }
+            Negative,
+            Positive
+        }
+        public enum ScoreEquality
+        {
+            GreaterThan,
+            LessThan
         }
 
-        public String EndDate
-        {
-            set { _commands[END_DATE] = value; }
-        }
+        private const string TAXONOMY_LABEL = "&q.enriched.url.enrichedTitle.taxonomy.taxonomy_.label=";
+        private const string CONCEPT_TEXT = "&q.enriched.url.concepts.concept.text=";
+        private const string KEYWORD_TEXT = "&q.enriched.url.enrichedTitle.keywords.keyword.text=";
+        private const string ENTITY_TEXT = "&q.enriched.url.enrichedTitle.entities.entity.text=";
+        private const string ENTITY_TYPE = "&q.enriched.url.enrichedTitle.entities.entity.type=";
+        private const string RELATION = "&q.enriched.url.enrichedTitle.relations.relation=";
+        private const string SENTIMENT = "&q.enriched.url.enrichedTitle.docSentiment=";
+        private const string SENTIMENT_POSITIVE = "type=positive,";
+        private const string SENTIMENT_NEGATIVE = "type=negative,";
 
-        public int Count
-        {
-            set { _commands[COUNT] = value > 0 ? value.ToString() : "0"; }
-        }
+        private List<string> _taxonomy;
+        private List<string> _concepts;
+        private List<string> _keyWords;
+        private List<string> _entityText;
+        private List<string> _entityType;
+        private List<string> _relations;
+        private int? _sentiment;
+        private SentimentType _sentimentType;
+        private ScoreEquality _scoreEquality;
+        private string _sentimentTypeString;
+        private string _sentimentScoreEquality;
+        private string _startDate;
+        private string _endDate;
+        private int? _count;
 
         public AlchemyAPI_NewsParams()
         {
-            _commands = new Dictionary<string, string>();
+            _taxonomy = new List<string>();
+            _concepts = new List<string>();
+            _keyWords = new List<string>();
+            _entityText = new List<string>();
+            _entityType = new List<string>();
+            _relations = new List<string>();
         }
 
-        public void initializeCommands()
+        public void addTaxonomy(string[] s)
         {
-            _commands[START_DATE] = "now";
-            _commands[END_DATE] = "now";
-            _commands[COUNT] = "0";
+            foreach(var str in s)
+            {
+                _taxonomy.Add(str);
+            }
+        }
+        public void addEntityText(string[] s)
+        {
+            foreach (var str in s)
+            {
+                _entityText.Add(str);
+            }
         }
 
-        public void setTaxonomy(params string[] s)
+        public void setCount(int c)
         {
-            _commands[TAXONOMY_LABEL] = String.Join("+", s);
+            _count = c;
         }
-
-        public void setReturn(params string[] s)
+        public void setStartDate(int amount = 0, char dateType = 'd')
         {
-            _commands[RETURN] = String.Join(",", s);
+            if (amount > 0)
+            {
+                _startDate = "&start=now-" + amount + dateType;
+            }
+            else
+            {
+                _endDate = "&start=now";
+            }
         }
-
-        public void setEntities(params Entity[] entities)
+        public void setEndDate(int amount = 0, char dateType = 'd')
         {
-            _commands[ENTITY_TEXT] = String.Join("+", entities.Select(e => e.Text.Replace(' ', '+')));
-            _commands[ENTITY_TYPE] = String.Join("+", entities.Select(e => e.Type));
+            if (amount > 0)
+            {
+                _endDate = "&end=now-" + amount + dateType;
+            }
+            else
+            {
+                _endDate = "&end=now";            
+            }
         }
 
         public override String getParameterString()
         {
-            return String.Format("{0}&{1}", base.getParameterString(), String.Join("&", _commands.Select(e => String.Format("{0}={1}", e.Key, e.Value))));
+            String retString = base.getParameterString();
+            if(_count != 0 || _count != null)
+            {
+                retString += "&count=" + _count.ToString();
+            }
+            if (_taxonomy.Count > 0)
+            {
+                retString += TAXONOMY_LABEL + string.Join("+", _taxonomy.ToArray());
+            }
+            if (_concepts.Count > 0)
+            {
+                retString += CONCEPT_TEXT + string.Join("+", _concepts.ToArray());
+            }
+            if (_keyWords.Count > 0)
+            {
+                retString += KEYWORD_TEXT + string.Join("+", _keyWords.ToArray());
+            }
+            if (_entityText.Count > 0)
+            {
+                retString += ENTITY_TEXT + string.Join("+", _entityText.ToArray());
+            }
+            if (_entityType.Count > 0)
+            {
+                retString += ENTITY_TYPE + string.Join("+", _entityType.ToArray());
+            }
+            if (_relations.Count > 0)
+            {
+                retString += RELATION + _relations;
+            }
+            if (_sentiment != null)
+            {
+                retString += SENTIMENT + "|" + _sentimentType + "score=" + _sentimentScoreEquality + _sentiment + "|";                
+            }
+            retString += _startDate + _endDate;
+            return retString;
         }
     }
-}
-
-public enum EntityType
-{
-    Person,
-    Company
-}
-
-public class Entity
-{
-    public EntityType Type { get; set; }
-    public string Text { get; set; }
-    public Entity(EntityType type, string text)
-    {
-        Type = type;
-        Text = text;
-    }
-}
-
-public enum SentimentType
-{
-    Negative,
-    Positive
-}
-
-public enum ScoreEquality
-{
-    GreaterThan,
-    LessThan
 }
