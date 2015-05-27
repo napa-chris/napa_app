ed497345b52eee086303aec39d5ee6514f9d6bfc
Chris Cummings <ccummings@napa.com>
2015-05-27 13:35:13 -0400

    retrieving data from mongodb

--- napa_app/napa_app/Program.cs
+++ napa_app/napa_app/Program.cs
@@ -29,22 +29,44 @@ namespace napa_app
             }
         }
 
+        public async static Task GetDocs()
+        {
+            var client = new MongoClient("mongodb://napa_user:Password0@ds031982.mongolab.com:31982/heroku_app37054022");
+            var db = client.GetDatabase("heroku_app37054022");
+            var collection = db.GetCollection<BsonDocument>("results");
+            var filter = Builders<BsonDocument>.Filter;
+            filter.ElemMatch("source.enriched.url.taxonomy", Builders<BsonDocument>.Filter.And(Builders<BsonDocument>.Filter.Gt("score", 0.5), Builders<BsonDocument>.Filter.Regex("label", "*.science.*")));
+            using (var cursor = await collection.Find(filter.ToBsonDocument()).ToCursorAsync())
+            {
+                while (await cursor.MoveNextAsync())
+                {
+                    foreach (var doc in cursor.Current)
+                    {
+                        Console.WriteLine(doc);
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             AlchemyAPI.AlchemyAPI alchemyObj = new AlchemyAPI.AlchemyAPI();
             alchemyObj.SetAPIKey("6b5aa8499e9a98215e57cf2ef6a0456654be777b");
             //alchemyObj.SetAPIKey("4c32fab89d526ce5e132a82213f68daf67c5d266");
 
-            AlchemyAPI_NewsParams prms = new AlchemyAPI_NewsParams();
-            prms.setOutputMode(AlchemyAPI_BaseParams.OutputMode.JSON);
-            prms.StartDate = "now-30d";
-            prms.EndDate = "now";
-            prms.Count = 10;
-            prms.setEntities(new Entity(EntityType.Person, "Elon Musk"));
-            prms.setReturn(ReturnOutputData.Url, ReturnOutputData.Title, ReturnOutputData.Taxonomy);
-            string json = alchemyObj.GetNews(prms);
+            //AlchemyAPI_NewsParams prms = new AlchemyAPI_NewsParams();
+            //prms.setOutputMode(AlchemyAPI_BaseParams.OutputMode.JSON);
+            //prms.StartDate = "now-1d";
+            //prms.EndDate = "now";
+            //prms.Count = 10;
+            //prms.setEntities(new Entity(EntityType.Person, "Elon Musk"));
+            //prms.setReturn(ReturnOutputData.Url, ReturnOutputData.Title, ReturnOutputData.Taxonomy);
+            //string json = alchemyObj.GetNews(prms);
 
-            var task = SaveResults(json);
+            //var task = SaveResults(json);
+            //task.Wait();
+
+            var task = GetDocs();
             task.Wait();
 
             Console.WriteLine("Done.");
